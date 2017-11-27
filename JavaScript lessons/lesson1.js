cb821e54752a00d8ad4213a158d4855c9c68016c
Andrey Gerasimuk <andrey.gerasimuk@gmail.com>
2017-11-27 17:04:33 +0700

    урок JavaScript 1
    переменные

---
+++ JavaScript lessons/lesson1.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var Tc = prompt("Введите температуру в градусах по Цельсию:");
+var Tf = ((9/5)*Tc+32);
+alert ("Температура в градусах по Фаренгейту:" + Tf);
+
+
+var admin;
+var name;
+name = "Василий";
+admin = name;
+alert (admin); // Василий
+
+alert (1000 + '100'); // 1000100
